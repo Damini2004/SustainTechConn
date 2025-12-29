c69bf2d6f376ee0cb128e105f610d559a89c3fe0
Divya Chachane <divyachachane06@gmail.com>
2025-12-29 08:54:14 +0000

    place this in center (_for element div_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -52,11 +52,11 @@ export function Footer() {
             </div>
             
             {/* Column 2: Navigation Links */}
-            <div className="space-y-4 flex flex-col items-center md:items-start">
+            <div className="space-y-4 flex flex-col items-center md:items-center">
                  <h2 className="font-headline text-3xl font-bold">
                     Quick <span className="text-accent">Links</span>
                 </h2>
-                <nav className="flex flex-col space-y-2 text-center md:text-left">
+                <nav className="flex flex-col space-y-2 text-center">
                     {navLinks.map((link) => (
                         link.isDropdown ? (
                             link.subLinks?.map(subLink => (
@@ -74,7 +74,7 @@ export function Footer() {
             </div>
 
             {/* Column 3: Social Links */}
-            <div className="space-y-4 flex flex-col items-center md:items-end">
+            <div className="space-y-4 flex flex-col items-center md:items-center">
                  <h2 className="font-headline text-3xl font-bold">
                     Follow <span className="text-accent">Us</span>
                 </h2>
